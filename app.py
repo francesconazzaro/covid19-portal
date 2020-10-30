9c3fe3ae7034f3bfe728a90abcc4157ba56db8c1
francesconazzaro <francesconazzaro89@gmail.com>
2020-10-30 18:38:56 +0100

    improve cache.

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import import_data
 
 
 st.beta_set_page_config(layout='wide')
-DATA = import_data.covid19()
+repo_reference = import_data.RepoReference()
+DATA = import_data.covid19(repo_reference)
 st.title('COVID-19: Situazione in Italia aggiornata al {}'.format(DATA['Italia'].index[-1].date()))
 
 
