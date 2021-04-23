c4b1344d992176adc1c7adf08d017968719155f3
francesconazzaro <francesconazzaro89@gmail.com>
2021-04-23 10:19:57 +0200

    change url

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import plugins
 
-plugins.google_analytics()
+#plugins.google_analytics()
 
 st.subheader(":flag-it: La dashboard è stata trasferita a questo indirizzo")
 st.subheader(":uk: The dashboard has been moved to this address")
-st.header("https://covid19-dashboard-it.appspot.com/")
+st.header("https://share.streamlit.io/francesconazzaro/covid19-portal/streamlit/app.py")
