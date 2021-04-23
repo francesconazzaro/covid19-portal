a8d218f1991008457b24feb0a4e227cde7ca0fab
francesconazzaro <francesconazzaro89@gmail.com>
2021-04-23 15:58:45 +0200

    redirect to main app

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: sh setup.sh && streamlit run app.py
-
+web: gunicorn wsgi:app
--- app.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-import plugins
-
-#plugins.google_analytics()
-
-st.subheader(":flag-it: La dashboard è stata trasferita a questo indirizzo")
-st.subheader(":uk: The dashboard has been moved to this address")
-st.header("https://share.streamlit.io/francesconazzaro/covid19-portal/streamlit/app.py")
